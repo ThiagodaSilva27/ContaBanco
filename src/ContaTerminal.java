91151d11d5181d5c0fa7fd16695d309b2ca3d634
Thisgo <thiagotwoseven@gmail.com>
2023-08-15 20:20:19 -0300

    feat: cadastrar, sacar e depositar

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -4,8 +4,25 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        float saldo = 0.0f;
         try {
+            float saldoAtual = cadastro(scanner);
+            System.out.println("Você deseja realizar um saque ou um deposito");
+            String resposta = scanner.next();
+            if (resposta.equals("saque")) {
+                saldoAtual = saque(scanner, saldoAtual);
+            } else if (resposta.equals("deposito")) {
+                saldoAtual = deposito(scanner, saldoAtual);
+            } else {
+                System.out.println("Digite uma função válida");
+            };
+            System.out.println("Seu saldo atualizado é de R$ "+saldoAtual);
+        } finally {
+            scanner.close();
+        }
+        
+    }
+
+    public static float cadastro(Scanner scanner) {
             System.out.println("Por favor, digite o número de sua Conta:");
             int numeroConta = scanner.nextInt();
 
@@ -16,12 +33,33 @@ public class ContaTerminal {
             String nomeCliente = scanner.next();
 
             System.out.println("Por favor, digite o saldo da sua conta:");
-            saldo = scanner.nextFloat();
+            float saldo = scanner.nextFloat();
 
             System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+ agenciaDigito +", conta "+numeroConta+" e seu saldo R$ "+saldo+", já está disponível para saque.");
-        } finally {
-            scanner.close();
+            return saldo;
+    }
+
+    public static float saque(Scanner scanner, Float saldoAtual) {
+        System.out.println("Digite o valor que deseja sacar ou sair");
+        float valor = scanner.nextFloat();
+        if (saldoAtual >= valor) {
+            float saldoAtualizado = saldoAtual - valor;
+            System.out.println("Saque de R$ "+valor+" realizado com sucesso!");
+            return saldoAtualizado;
         }
-        
+        System.out.println("O saque não foi realizado, tente novamente mais tarde.");
+        return saldoAtual;
+
+    }
+
+    public static float deposito(Scanner scanner, Float saldoAtual) {
+        System.out.println("Digite o valor que deseja depositar: ");
+        float valor = scanner.nextFloat();
+        if (valor > 0) {
+            float saldoAtualizado = valor + saldoAtual;
+            return saldoAtualizado;
+        }
+        System.out.println("Depósito não realizado, tente novamente mais tarde.");
+        return saldoAtual;
     }
 }
